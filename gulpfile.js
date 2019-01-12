b0c7f3b5b87ced6ad8bff98579de7c4248a865fc
philipp <fkolesnik@gmail.com>
2019-01-12 21:12:44 +0300

    testing babel 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 gulp.task('js', () =>
     gulp.src('_scripts/*.js')
+        .pipe(concat('bundle.js'))
         .pipe(babel({
             presets: ['@babel/env']
         }))
+        .pipe(uglify())
         .pipe(gulp.dest('assets/js'))
 );
