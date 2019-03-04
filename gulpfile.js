436cd7e70cadd34a992d11c1b36175a06cbe22d4
Pressfeed Dev <pressfeed.dev@yandex.ru>
2019-03-04 10:53:14 +0000

    Deploy from Forestry.io

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+const child = require('child_process');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const util = require('gulp-util');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const mincss = require('gulp-minify-css');
+const shell = require('gulp-shell');
+
+const jekyllLogger = (buffer) => {
+    buffer.toString()
+        .split(/\n/)
+        .forEach((message) => util.log('Jekyll: ' + message));
+};
+
+function onError(err) {
+    console.log(err);
+    this.emit('end');
+}
+
+//////////////////////// DEV ////////////////////////
+
+gulp.task('scss', function () {
+    return gulp.src('assets/css/_sass/app.scss')
+        .pipe(sass())
+        .on('error', onError)
+        .pipe(autoprefixer())
+        .pipe(mincss({keepBreaks: false}))
+        .pipe(gulp.dest('assets/css/'))
+});
+
+gulp.task('js', () =>
+    gulp.src('assets/js/_scripts/**/*.js')
+        .pipe(concat('bundle.js'))
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('assets/js'))
+);
+
+gulp.task('develop', function () {
+    gulp.watch("assets/css/_sass/**/*.scss", ["scss"]);
+    gulp.watch("assets/js/_scripts/**/*.js", ["js"]);
+    const jekyllServe = child.spawn('bundle', ['exec', 'jekyll', 'serve']);
+    jekyllServe.stdout.on('data', jekyllLogger);
+    jekyllServe.stderr.on('data', jekyllLogger);
+});
+
+
+//////////////////////// BUILD ////////////////////////
+
+gulp.task('clean', function () {
+    const jekylClean = child.spawn('bundle', ['exec', 'jekyll', 'clean']);
+    jekylClean.stdout.on('data', jekyllLogger);
+    jekylClean.stderr.on('data', jekyllLogger);
+});
+
+gulp.task('build', ['js', 'scss', 'clean'], function () {
+    gulp.src('/', {read: false})
+        .pipe(shell([
+            'JEKYLL_ENV=production bundle exec jekyll build'
+        ]))
+});
